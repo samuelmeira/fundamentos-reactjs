31c06c9082c6a18b4f6225ee0b4b92429243b5ba
Samuel Meira <samuelleandmeira@hotmail.com>
2020-07-15 19:05:14 -0300

    add style to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './style';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './style';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/21692790?s=460&v=4"
+            alt="Samuel Meira"
+          />
+          <div>
+            <strong>samuelmeira/frontend-reactjs</strong>
+            <p>Description Description Description Description Description </p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/21692790?s=460&v=4"
+            alt="Samuel Meira"
+          />
+          <div>
+            <strong>samuelmeira/frontend-reactjs</strong>
+            <p>Description Description Description Description Description </p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/21692790?s=460&v=4"
+            alt="Samuel Meira"
+          />
+          <div>
+            <strong>samuelmeira/frontend-reactjs</strong>
+            <p>Description Description Description Description Description </p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
